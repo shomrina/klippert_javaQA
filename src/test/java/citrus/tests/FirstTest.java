f26ac9b31364d46d5c491304832efd07e48c52fc
Klippert Marina <milagrous@gmail.com>
2021-07-05 19:15:16 +0300

    hw part: four contracts - user not found, create, update and delete user

--- src/test/java/citrus/tests/FirstTest.java
+++ src/test/java/citrus/tests/FirstTest.java
@@ -7,7 +7,10 @@ import com.consol.citrus.dsl.testng.TestNGCitrusTestRunner;
 import com.consol.citrus.http.client.HttpClient;
 import com.consol.citrus.message.MessageType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import static org.springframework.http.HttpStatus.*;
 
 public class FirstTest extends TestNGCitrusTestRunner {
 
@@ -16,13 +19,114 @@ public class FirstTest extends TestNGCitrusTestRunner {
     private TestContext context; //для работы с контекстом нужно цитрусовое
 
 
+    //Homework
+    @Test(description = "Get not existing user")
+    @CitrusTest
+    public void userNotFoundTest() {
+        http(httpActionBuilder -> httpActionBuilder
+                .client(restClient)
+                .send()
+                .get("/users/" + context.getVariable("notExistingUserId"))
+        );
 
+        http(httpActionBuilder -> httpActionBuilder
+                .client(restClient)
+                .receive()
+                .response()
+                .status(NOT_FOUND)
+                .payload("{}")
+        );
+
+    }
+
+    @Test(description = "create user")
+    @CitrusTest
+    public void createUserTest() {
+        http(httpActionBuilder -> httpActionBuilder
+                .client(restClient)
+                .send()
+                .post("/users")
+                .payload("{\n" +
+                        "    \"name\": \"morpheus\",\n" +
+                        "    \"job\": \"leader\"\n" +
+                        "}")
+        );
+
+        http(httpActionBuilder -> httpActionBuilder
+                .client(restClient)
+                .receive()
+                .response()
+                .messageType(MessageType.JSON)
+                .status(CREATED)
+                .payload("{\n" +
+                        "    \"name\": \"morpheus\",\n" +
+                        "    \"job\": \"leader\",\n" +
+                        "    \"id\": \"@isNumber()@\",\n" +
+                        "    \"createdAt\": \"@assertThat(notNullValue())@\"\n" +
+                 "}")
+        );
+
+    }
+
+    @Test(description = "update user")
+    @CitrusTest
+    public void updateUserTest() {
+        http(httpActionBuilder -> httpActionBuilder
+                .client(restClient)
+                .send()
+                .put("/users/" + context.getVariable("userId"))
+                .payload("{\n" +
+                        "    \"name\": \"morpheus\",\n" +
+                        "    \"job\": \"zion resident\"\n" +
+                        "}")
+        );
+
+        http(httpActionBuilder -> httpActionBuilder
+                .client(restClient)
+                .receive()
+                .response()
+                .messageType(MessageType.JSON)
+                .status(OK)
+                .payload("{\n" +
+                        "    \"name\": \"morpheus\",\n" +
+                        "    \"job\": \"zion resident\",\n" +
+                        "    \"updatedAt\": \"@assertThat(notNullValue())@\"\n" +
+                        "}")
+        );
+
+    }
+
+    @Test(description = "delete user")
+    @CitrusTest
+    public void deleteUserTest() {
+        http(httpActionBuilder -> httpActionBuilder
+                .client(restClient)
+                .send()
+                .delete("/users/" + context.getVariable("userId"))
+        );
+
+        http(httpActionBuilder -> httpActionBuilder
+                .client(restClient)
+                .receive()
+                .response()
+                .status(NO_CONTENT)
+        );
+
+    }
+
+    @BeforeMethod
+    @CitrusTest
+    public void setContext() {
+        this.context = citrus.createTestContext();      //контекст будет виден методу
+    }
+
+    //classwork
     @Test(description = "get information about user")
     @CitrusTest   //необходимо чтобы цитрус распознал это как цитрус-тест
     public void getTestsAction() {
-        this.context = citrus.createTestContext();      //контекст будет виден методу
+     //   this.context = citrus.createTestContext();      //контекст будет виден методу
 
-        //достать и залогировать вериэблс
+        //достать и залогировать вериэблс. Пример работы с переменными цитруса!
         context.setVariable("value", "!!!!!!!!!!!");
         echo(context.getVariable("value"));
         echo(context.getVariable("var"));   //получение переменной по цитрус-контекст
@@ -63,7 +167,6 @@ public class FirstTest extends TestNGCitrusTestRunner {
     @Test(description = "get information about user - fail")
     @CitrusTest   //необходимо чтобы цитрус распознал это как цитрус-тест
     public void getTestsAction2() {
-        this.context = citrus.createTestContext();      //контекст будет виден методу
 
         //сформируем экшн с помощью вызова билдера хттп (request)
         http(httpActionBuilder -> httpActionBuilder
