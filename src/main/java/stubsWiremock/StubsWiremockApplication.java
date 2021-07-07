6f63d8de50dd027664fb32a3c2e9e46df37eaa4c
Klippert Marina <milagrous@gmail.com>
2021-07-07 22:04:14 +0300

    classwork wiremock

---
+++ src/main/java/stubsWiremock/StubsWiremockApplication.java
@@ -0,0 +1,12 @@
+package stubsWiremock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StubsWiremockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StubsWiremockApplication.class, args);
+    }
+}
